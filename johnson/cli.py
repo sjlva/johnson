768213e83630a2fefe97e33bce6466d37e239ebf
sjlva <rtsilva@bi4all.pt>
2023-08-17 22:25:57 +0100

    format cli.py

--- johnson/cli.py
+++ johnson/cli.py
@@ -1,5 +1,4 @@
 import typer
-
 from rich import print_json
 from rich.console import Console
 from typer import Typer
@@ -7,14 +6,12 @@ from typing_extensions import Annotated
 
 from johnson.read_json import read_json_file
 
-# from read_json import read_json_file
-
 console = Console()
 app = Typer()
 
 
 @app.command()
-def johnson(file: Annotated[str, typer.Option("--file", "-f")]):
+def johnson(file: Annotated[str, typer.Option('--file', '-f')]):
 
     data = read_json_file(file)
 
