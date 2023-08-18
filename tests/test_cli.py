225becae85eb837f9b7cd3f11498d31c18e50263
sjlva <rtsilva@bi4all.pt>
2023-08-18 02:56:46 +0100

    update tests to new raises

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,3 +1,4 @@
+from pytest import raises
 from typer.testing import CliRunner
 
 from johnson.cli import app
@@ -12,15 +13,24 @@ def test_app_returns_0():
     assert result.exit_code == 0
 
 
-def test_json_contains_keys():
+def test_json_contains_keys_and_values():
     file = 'sample_jsons/example_1.json'
     result = runner.invoke(app, ['--file', file])
 
     assert 'key_1' in result.stdout
+    assert 'value_1' in result.stdout
 
 
-def test_json_contains_values():
-    file = 'sample_jsons/example_1.json'
+def test_json_is_none():
+    file = ''
+
     result = runner.invoke(app, ['--file', file])
 
-    assert 'value_1' in result.stdout
+    assert result.exit_code == 1
+
+
+def test_json_does_not_exist():
+    file = 'sample_jsons/exle_1.json'
+    result = runner.invoke(app, ['--file', file])
+
+    assert result.exit_code == 1
