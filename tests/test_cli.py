9cdbe41b17a1e1bbd92bf54875703c4e99925406
sjlva <rtsilva@bi4all.pt>
2023-08-18 02:41:03 +0100

    refactor tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -6,21 +6,21 @@ runner = CliRunner()
 
 
 def test_app_returns_0():
-    file = '../sample_jsons/example_1.json'
+    file = 'sample_jsons/example_1.json'
     result = runner.invoke(app, ['--file', file])
 
     assert result.exit_code == 0
 
 
 def test_json_contains_keys():
-    file = '../sample_jsons/example_1.json'
+    file = 'sample_jsons/example_1.json'
     result = runner.invoke(app, ['--file', file])
 
     assert 'key_1' in result.stdout
 
 
 def test_json_contains_values():
-    file = '../sample_jsons/example_1.json'
+    file = 'sample_jsons/example_1.json'
     result = runner.invoke(app, ['--file', file])
 
     assert 'value_1' in result.stdout
--- tests/test_read_json.py
+++ tests/test_read_json.py
@@ -2,36 +2,12 @@ import json
 
 from pytest import raises
 
-from johnson.read_json import check_if_file_exists, read_json_file
-
-
-def test_file_not_exists():
-
-    path_to_file = '../inexistent_path/inexistent_file.json'
-    file_exists = check_if_file_exists(path_to_file)
-
-    assert file_exists is False
-
-
-def test_file_exists():
-
-    path_to_file = '../sample_jsons/example_1.json'
-    file_exists = check_if_file_exists(path_to_file)
-
-    assert file_exists is True
-
-
-def test_read_inexistent_json():
-
-    path_to_file = '../inexistent_path/inexistent_file.json'
-    json_file = read_json_file(path_to_file)
-
-    assert json_file is None
+from johnson.read_json import read_json_file
 
 
 def test_read_valid_json():
 
-    path_to_file = '../sample_jsons/example_1.json'
+    path_to_file = 'sample_jsons/example_1.json'
     json_file = read_json_file(path_to_file)
 
     assert json_file == {'key_1': 'value_1'}
@@ -39,7 +15,7 @@ def test_read_valid_json():
 
 def test_read_invalid_json():
 
-    path_to_file = '../sample_jsons/example_1_invalid.json'
+    path_to_file = 'sample_jsons/example_1_invalid.json'
     error_message = 'Expecting value: line 2 column 12 (char 13)'
 
     with raises(json.decoder.JSONDecodeError) as error:
