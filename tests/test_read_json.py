e5bb8204a94f0de5fa6322389f546c083810a425
sjlva <rtsilva@bi4all.pt>
2023-08-18 03:08:56 +0100

    add relative path to tests

--- tests/test_read_json.py
+++ tests/test_read_json.py
@@ -1,13 +1,16 @@
 import json
+from pathlib import Path
 
 from pytest import raises
 
 from johnson.read_json import read_json_file
 
+test_data_dir = Path(__file__).parent / 'sample_jsons'
+
 
 def test_read_valid_json():
 
-    path_to_file = 'sample_jsons/example_1.json'
+    path_to_file = f'{test_data_dir}/example_1.json'
     json_file = read_json_file(path_to_file)
 
     assert json_file == {'key_1': 'value_1'}
@@ -15,7 +18,7 @@ def test_read_valid_json():
 
 def test_read_invalid_json():
 
-    path_to_file = 'sample_jsons/example_1_invalid.json'
+    path_to_file = f'{test_data_dir}/example_1_invalid.json'
     error_message = 'Expecting value: line 2 column 12 (char 13)'
 
     with raises(json.decoder.JSONDecodeError) as error:
